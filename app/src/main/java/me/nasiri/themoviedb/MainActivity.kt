d230a58ba48e312c9ee060976e29acad942f1428
Matin <mnasirii829@gmail.com>
2024-05-28 15:26:25 +0330

    initial commit

---
+++ app/src/main/java/me/nasiri/themoviedb/MainActivity.kt
@@ -0,0 +1,30 @@
+package me.nasiri.themoviedb
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import me.nasiri.themoviedb.ui.theme.ThemoviedbTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ThemoviedbTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Text(
+                        text = stringResource(id = R.string.app_name),
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
