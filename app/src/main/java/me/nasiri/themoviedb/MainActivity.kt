a64828c6564d0f3dd0f5d6ac9882eb1f449dd298
Matin <mnasirii829@gmail.com>
2024-05-29 11:07:48 +0330

    Added network module and hilt library

--- app/src/main/java/me/nasiri/themoviedb/MainActivity.kt
+++ app/src/main/java/me/nasiri/themoviedb/MainActivity.kt
@@ -10,8 +10,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import dagger.hilt.android.AndroidEntryPoint
 import me.nasiri.themoviedb.ui.theme.ThemoviedbTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
